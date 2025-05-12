b4baf03d38e727eac0aec8134ee3a2bc23a5f34c
L.J.Cammish <l.j.cammish@student.saintkentigern.com>
2025-05-13 11:48:47 +1200

    started work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use ollama_rs::models::ModelOptions;
+use ollama_rs::Ollama;
+use ollama_rs::generation::completion::request::GenerationRequest;
+
+#[tokio::main]
+async fn main() {
+    let prompt = "What is the capital of France?";
+    let ollama = Ollama::default();
+    
+    let request = GenerationRequest::new(String::from("deepseek-r1:1.5b"), prompt)
+        .options(ModelOptions::default())
+        .system("You are a helpful AI assistant who has a strong devotion to the truth.\nYou are in a school environment, and you are to adhere to certain policies related to this. Begin talking now.");
+    match ollama.generate(request).await {
+        Ok(response) => println!("Response: {:?}", response.response),
+        Err(err) => eprintln!("Generation Error: {:?}", err),
+    }
 }
