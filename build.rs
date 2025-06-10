51306f03ca51a9ace6126960428629daf6c3aa09
L.J.Cammish <l.j.cammish@student.saintkentigern.com>
2025-06-11 09:57:37 +1200

    fixed issues with the new json dirs

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use {
     std::{
-        env, fs, io, path::{Path, PathBuf}
+        collections::HashMap, env, fs, io, ops::Deref, path::{Path, PathBuf}
     },
     winresource::WindowsResource,
 };
@@ -16,22 +16,22 @@ fn main() -> io::Result<()> {
     // Copy assets folder to output directory
     let out_dir = env::var("OUT_DIR").unwrap();
     let target_dir = Path::new(&out_dir).ancestors().nth(3).unwrap();
-    let assets_dir: Vec<PathBuf> = vec![ 
-        Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("config"),
-        Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("output"),
-        Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("assets")
-    ];
+    let dirs: HashMap<&str, PathBuf> = vec![ 
+        ("config", Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("config")),
+        ("output", Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("output")),
+        ("assets", Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("assets")),
+    ].into_iter().collect();
     
-    for dir in assets_dir.iter() { 
-        if dir.exists() {
-            let target_assets = target_dir.join("assets");
-            if target_assets.exists() {
-                fs::remove_dir_all(&target_assets)?;
+    for dir in dirs.iter() { 
+        if dir.1.deref().exists() {
+            let target = target_dir.join(*dir.0);
+            if target.exists() {
+                fs::remove_dir_all(&target)?;
             }
-            fs::create_dir_all(&target_assets)?;
-            for entry in fs::read_dir(dir)? {
+            fs::create_dir_all(&target)?;
+            for entry in fs::read_dir(dir.1.deref())? {
                 let entry = entry?;
-                let target_path = target_assets.join(entry.file_name());
+                let target_path = target.join(entry.file_name());
                 fs::copy(entry.path(), target_path)?;
             }
         }
